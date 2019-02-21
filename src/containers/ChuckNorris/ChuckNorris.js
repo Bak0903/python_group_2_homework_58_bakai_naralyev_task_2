5d6085bdc302bb4f2a312b4acdf43b40445f3b71
bak0903 <bak0903@mail.ru>
2019-02-21 18:58:29 +0600

    use promise

--- src/containers/ChuckNorris/ChuckNorris.js
+++ src/containers/ChuckNorris/ChuckNorris.js
@@ -10,13 +10,25 @@ class ChuckNorris extends Component {
     };
 
     getJoke = () => {
-        const P_UR = 'https://api.chucknorris.io/jokes/random';
-        fetch(P_UR).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Something went wrong with network request');
-        }).then(posts => {this.setState({posts: posts})}).catch(error => {console.log(error);});
+        const P_URL = 'https://api.chucknorris.io/jokes/random';
+        let Array = [];
+        let count = 5;
+        for (let i = 0; Array.length < count; i++) {
+            let promise = fetch(P_URL).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Something went wrong with network request');
+            }).catch(error => {
+                console.log(error);
+            });
+            Array.push(promise)
+        }
+        Promise.all(Array).then(posts => {
+            this.setState({posts: posts});
+        }).catch(error => {
+            console.log(error);
+        })
     };
 
     componentDidMount() {
@@ -26,10 +38,12 @@ class ChuckNorris extends Component {
     render() {
         return (
             <div className="about_chuck">
-                <Joke
-                    key={this.state.posts.id}
-                    text={this.state.posts.value}
-                />
+                {this.state.posts.map(joke => (
+                        <Joke
+                            key={joke.id}
+                            text={joke.value}
+                        />
+                    ))}
                 <Button
                 Click = {this.getJoke}
             />
